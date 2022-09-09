cb2ee7606af23631f7aa7ef07279b4bc50bb7d8e
faith-muraya <faith.muraya04@gmail.com>
2022-09-09 04:51:01 -0700

    Code 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Prints the size
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+char a;
+int b;
+long int c;
+long long int d;
+float e;
 
-	printf("size of a char: %d byte(s)\n",of(a));
-	printf("size of an int: %d byte(s)\n",size of(b));
-	printf("size of a long int: %d byte(s)\n",size of(c))
-	printf("size of a long long int: %d byte(s)\n",size of(d));
-	printf("size o a float: %d byte(s)\n",size of(e));
-	return (0);
+printf("size of a char: %lu byte(s)\n",(unsigned long)size of(a));
+printf("size of an int: %lu byte(s)\n",(unsigned long)size of(b));
+printf("size of a long int: %lu byte(s)\n",(unsigned long)size of(c));
+printf("size of a long long int: %lu byte(s)\n",(unsigned long)size of(d));
+printf("size o a float: %lu byte(s)\n",(unsigned long)size of(e));
+return (0);
 }	
