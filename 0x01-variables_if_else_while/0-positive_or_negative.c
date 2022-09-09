d023cc4396cacde6cfd901a26ba5902ca51a4265
faith-muraya <faith.muraya04@gmail.com>
2022-09-09 10:13:26 -0700

    Task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,31 +5,22 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success/correct)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
+
 int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if (n == 0)
-{
-printf("%i is zero\n",n);
-}
-
+if(n > 0)
+	printf("%d is positive\n",n);
+else if (n == 0)
+	printf("%d is zero\n",n);
 else
-{
-printf("%i is negative\n",n);	
-}
-
-else
-{
-printf("%i is positive\n",n);
-}
+	printf("%i is negative\n",n);
 
 return (0);
 }
-
-	
