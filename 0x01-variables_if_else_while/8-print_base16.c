559801ec5d0e0850cdd1c8711333f9e92f233020
faith-muraya <faith.muraya04@gmail.com>
2022-09-10 14:17:42 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+int d = 48
+char c = 'a';
+
+while (d < 58)
+{
+putchar(c);
+c++
+}
+putchar('\n');
+
+return (0);
+}
