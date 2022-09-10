2b08bc2090f31be76900b56ffa21765bbabf589c
faith-muraya <faith.muraya04@gmail.com>
2022-09-10 13:49:22 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:Alway 0 (Success)
+ */
+int main(void)
+{
+int d = 48;
+
+while (d < 58)
+{
+putchar(d);
+d++;
+}
+putchar('\n');
+
+return (0)
+}
+
