8bbcc3a211be4a9d6a0c3dedae67a9cf834cc951
faith-muraya <faith.muraya04@gmail.com>
2022-09-10 03:11:35 -0700

    Task four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Print the string in the put function
- *
- *Decription: using the main function
- *this program prints "Programming is like building a multilingual puzzle
+ * main - main block
  * Return: 0
  */
 int main(void)
