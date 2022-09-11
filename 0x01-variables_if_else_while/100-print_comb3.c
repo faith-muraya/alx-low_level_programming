19407213a2a045cf806977947af5a4e3ec0619e9
faith-muraya <faith.muraya04@gmail.com>
2022-09-11 10:01:53 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,8 @@ int main(void)
 int d = 48, a = 48;
 
 while(d < 58)
-{a =48);
+{
+a = (48);
 while (a < 58)
 {
 if (d ! =a && d < a)
@@ -18,7 +19,7 @@ if (d ! =a && d < a)
 putchar(d);
 putchar(a);
 
-if (!d == 56 && a == 57)
+if (! d == 56 && a == 57)
 {
 putchar(',');
 putchar(' ');
