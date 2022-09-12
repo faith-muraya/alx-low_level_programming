99d55bbddf466574f0daa990baa1b1ed49e61e0d
faith-muraya <faith.muraya04@gmail.com>
2022-09-12 12:52:04 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,28 +7,24 @@
  */
 int main(void)
 {
-	int i, j;
+int digit1, digit2;
 
-	for (i = 0; i <= '9'; i++)
-	{
-		for (j = 1 ; j <= '9'; j++)
-		{				
-	
-			if (j < i)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
+for (digit1 = 0; digit1 < 9; digit1++)
+{
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+{
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
 
-				if (i!=8)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		
-		}
-	}
+if (digit1 == 8 && digit2 == 9)
+continue;
+
+putchar(',');
+putchar(' ');
+}
+}
 
 putchar('\n');
+
 return (0);
 }
