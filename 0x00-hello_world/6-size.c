741d32c850a94bd075b8dab5e28efd84010f809f
faith-muraya <faith.muraya04@gmail.com>
2022-09-12 10:53:35 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,6 +19,5 @@ printf("size of an int: %d byte(s)\n", sizeof(b));
 printf("size of a long int: %d byte(s)\n", sizeof(c));
 printf("size of a long long int: %d byte(s)\n", sizeof(d));
 printf("size of a float: %d byte(s)\n", sizeof(e));
-
 return (0);
 }	
