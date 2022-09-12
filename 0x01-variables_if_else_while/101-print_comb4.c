cd4bb27947c7dfa74abb9b6f2a90ed313a1dbfce
faith-muraya <faith.muraya04@gmail.com>
2022-09-12 12:31:37 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,8 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-  int main(void)
- {
+int main(void)
+{
 int digit1, digit2, digit3;
 
 for (digit1 = 0; digit1 < 8; digit1++)
@@ -26,17 +26,9 @@ putchar(',');
 putchar(' ');
 }
 }
-	 
 }
 
 putchar('\n');
 
 return (0);
-}
-
-
-
-
-
-	
-	
+}	
