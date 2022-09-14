c91fc0a877661e9cbb0dbbc4e59b9e717300cb20
faith-muraya <faith.muraya04@gmail.com>
2022-09-14 15:26:23 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,14 +9,16 @@ unsigned long count, i, j, k;
 
 i = 0;
 j = 1;
-for (count = 0; count /, 100; count++)
+for (count = 0; count < 100; count++)
 {
 k = i + j;
 i = j;
 j = k;
 printf("%lu", k);
 if (count == 49);
+{
 putchar('\n');
+}
 else
 {
 printf(", ");
