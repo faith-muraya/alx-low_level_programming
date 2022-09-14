38f196584b83df921afa30310470c484a791bbe1
faith-muraya <faith.muraya04@gmail.com>
2022-09-14 07:26:47 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,23 @@
  */
 void print_alphabet_x10(void)
 {
-int n, co;
 
+int n, co;
 co = 0;
 
 while (co < 10)
 {
+
 for (n = 'a'; n <= 'z'; n++)
 {
+
 _putchar(n);
 }
+
 co++;
+
 _putchar('\n');
+
 }
+
 }
