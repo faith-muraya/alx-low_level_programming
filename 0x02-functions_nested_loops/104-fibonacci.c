10b1da816fdaccf853fa6f02e04683c2833fbcc0
faith-muraya <faith.muraya04@gmail.com>
2022-09-14 15:05:14 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,125 @@
 #include <stdio.h>
 
 /**
- * main - Prints the sum of Even Fibonacci numbers
- * less tan 4000000.
+ * main - Prints the first 98 Fibonacci numbers, starting with 1 and 2, separated by a comma followed by a space.
  *
- * Return: Nothing!
+ * Return: Always 0.
+ *
  */
 int main(void)
 {
-int i = 0;
-long j = 1, k = 2, sum = k;
+int count;
+unsigned long fib 1 = 0, fib2 =1, sum;
+unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+unsigned long half1, half2;
 
-while (k + j < 4000000)
+for (count = 0; count < 92; count++)
 {
-k += j;
+sum = fib1 + fib2;
+printf("%lu, ", sum);
 
-if (k % 2 == 0)
-sum += k;
-
-j = k - j;
-
-++i;
+fib1 = fib2;
+fib2 = sum;
 }
 
-printf("%ld\n", sum);
+Skip to content
+
+bdbaraban
+
+/
+
+holbertonschool-low_level_programming
+
+Public
+
+Code
+
+Issues
+
+2
+
+Pull requests
+
+2
+
+Actions
+
+Projects
+
+More
+
+holbertonschool-low_level_programming/0x02-functions_nested_loops/104-fibonacci.c
+
+@234761
+
+234761 Cleaned code
+
+ History
+
+  1 contributor
+
+  56 lines (48 sloc)  1.04 KB
+
+  /*
+   *
+   *  * File: 104-fibonacci.c
+   *
+   *   * Auth: Brennan D Baraban
+   *
+   *    */
+
+
+
+#include <stdio.h>
+
+
+
+  /**
+   *
+   *  * main - Prints the first 98 Fibonacci numbers, starting with 1 and 2, separated by a comma followed by a space.
+   *
+   *  Return:Always 0.
+   */
+  int main(void)
+{
+int count;
+unsigned long fib1 = 0, fib2 = 1, sum;
+unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+unsigned long half1, half2;
+
+for (count = 0; count < 92; count++)
+{
+sum = fib1 + fib2;
+printf("%lu, ", sum);
+
+fib1 = fib2;
+fib2 = sum;
+}
+
+fib1_half1 = fib1 / 10000000000;
+fib2_half1 = fib2 / 10000000000;
+fib1_half2 = fib1 % 10000000000;
+fib2_half2 = fib2 % 10000000000;
+
+for (count = 93; count < 99; count++)
+{
+half1 = fib1_half1 + fib2_half1;
+half2 = fib1_half2 + fib2_half2;
+if (fib1_half2 + fib2_half2 > 9999999999)
+{
+half1 += 1;
+half2 %= 10000000000;
+}
+
+printf("%lu%lu", half1, half2);
+if (count != 98)
+printf(", ");
+
+fib1_half1 = fib2_half1;
+fib1_half2 = fib2_half2;
+fib2_half1 = half1;
+fib2_half2 = half2;
+}
+printf("\n");
 return (0);
 }
