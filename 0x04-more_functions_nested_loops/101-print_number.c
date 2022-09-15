48ce132db90274ac0bb603b864d830e60ee96170
faith-muraya <faith.muraya04@gmail.com>
2022-09-15 12:06:10 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,20 +4,22 @@
 /**
  * print_number - Prints a number
  * @n: The number to print
+ *
+ * Return: returns 0
  */
 
 void print_number(int n)
 {
-unsigned int num = n;
+	unsigned int num = n;
 
-if (n < 0)
-{
-putchar('-');
-num = -num;
-}
-if (num > 9)
-{
-print_number(num / 10);
-}
-putchar(num % 10 + '0');
+	if (n < 0)
+	{
+		putchar('-');
+		num = -num;
+	}
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	putchar(num % 10 + '0');
 }
