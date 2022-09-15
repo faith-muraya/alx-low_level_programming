126da3ecac25a56b084176256a4bdd8e01d0ec99
faith-muraya <faith.muraya04@gmail.com>
2022-09-15 06:31:36 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,11 +5,15 @@
  * @c: character to check.
  * Return: 1 i c is uppercase, otherwise return 0.
  */
+
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
 {
 return (1);
+}
 else
+{
 return (0);
+}
 }
