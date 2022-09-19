42b79b84d7bf2ed677e8f73300b7dda4a1998921
faith-muraya <faith.muraya04@gmail.com>
2022-09-19 03:33:28 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,12 +6,13 @@
  * @str: input tring to print.
  *
  * Description: prints a string
+ * On success: return no error
  */
 void _puts(char *str)
 {
 	int i = 0; 
 
-	while (*(str +i) != '\0')
+	while (*(str + i) != '\0')
 	{
 		putchar(*(str + i));
 		i++;
