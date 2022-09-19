f23dab239faf74303ac8f35e9f65c4f98937c815
faith-muraya <faith.muraya04@gmail.com>
2022-09-19 09:12:27 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,14 +7,28 @@
  */
 void puts_half(char *str)
 {
-	int x, y, i;
+	int j =0;
+	int k;
 
-	x = strlen(str);
-	if (x % 2 == i)
-		y = x / 2 + l;
+	while (str[j] ! = '\0')
+	{
+		j++
+	}
+
+	if (j % 2 == 1)
+	{
+		k = (j - 1) / 2;
+		k + = 1;
+	}
 	else
-		y = x / 2;
-	for (i = y; i < x; i++)
-		_putchar(str[i]);
+	{
+		k = j / 2;
+	}
+
+	for (; k < j; k++)
+	{
+		_putchar(str[k]);
+	}
+
 	_putchar('\n');
 }
