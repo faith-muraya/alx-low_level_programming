138ed307ccdd6532e297e0f44575fde40a35d976
faith-muraya <faith.muraya04@gmail.com>
2022-09-22 09:23:24 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
 /**
  * _strcmp - Function which compare two strings
  * @s1: first string
- * @s2: second string
+ * @s2:second string
  * Return:
  *  		returns zero if s1 == s2
  *  		returns negative number if s1 < s2
