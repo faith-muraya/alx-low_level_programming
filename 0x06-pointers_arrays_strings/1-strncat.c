09fef860672f1fb5e072629e92110570e3296ab9
faith-muraya <faith.muraya04@gmail.com>
2022-09-22 04:49:29 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,26 +7,16 @@
  * @n: The limit of the concatenation
  * Return: A pointer to the resulting string dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int dlen = 0, j = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[dlen])
-	{
-		dlen++;
-	}
+	while (dest[index++])
+		dest_len++;
 
-	while (j < n && src[j]);
-	{
-		dest[dlen] = src[j];
-		dlen++;
-		j++;
-	}
-
-	dest[dlen + n + 1] = '\0';
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
-	
-
-
