76bbbba704c1e9b42bdc72d9f0fb4d7210c3457d
faith-muraya <faith.muraya04@gmail.com>
2022-09-22 07:06:15 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,16 +1,15 @@
 #include "main.h"
-
 /**
  *_strcmp - Function which compare two strings and
  *@s1: first string
  *s2: The second string
  *Return:
- *  			returns zero if s1 == s2
- *  			returns negative number if s1 < s2
- *  			returns positive number if s1 > s2
+ *  		returns zero if s1 == s2
+ *  		returns negative number if s1 < s2
+ *  		returns positive number if s1 > s2
  */
 
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
 	int i = 0, diff = 0;
 
