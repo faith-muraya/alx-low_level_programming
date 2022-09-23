be58e079d0a97c2d29458112bf8244e85e0cd3b5
faith-muraya <faith.muraya04@gmail.com>
2022-09-23 03:23:37 -0700

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,7 +8,7 @@
  */
 char *rot13(char *str)
 {
-	int index1 = 0, index2;
+	int indx1 = 0, indx2;
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R','S', 'T', 'U', 'V', 'W', 'X','Y', 'Z', 'a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z'};
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S','T', 'U', 'V', 'W', 'X', 'Y','Z', 'A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', 'K','L', 'M', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w','x', 'y', 'z', 'a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i','j', 'k', 'l', 'm'};
 
@@ -23,7 +23,7 @@ char *rot13(char *str)
 			}
 		}
 
-		index1++;
+		indx1++;
 	}
 
 	return (str);
