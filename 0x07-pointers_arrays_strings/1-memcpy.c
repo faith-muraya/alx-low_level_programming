7d7f93443a9be70d746cc78a2c835e16f9ab709f
faith-muraya <faith.muraya04@gmail.com>
2022-09-26 06:29:46 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,14 +9,12 @@
  * Return: pointer to the copied memory block
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	for (; i <n; i++)
-	{
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
-	}
 
 	return (dest);
 }
