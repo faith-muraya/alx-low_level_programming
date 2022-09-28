8231d232c1594246b2d0fb3fd983dbdeb5fe4e17
faith-muraya <faith.muraya04@gmail.com>
2022-09-28 04:05:10 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,14 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s !== '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
 	}
 	else
 	{
-		_putchar('\n');
+		_putchar(s[0]);
+		_puts_recursion(s + 1)
+		  
 	}
 }
