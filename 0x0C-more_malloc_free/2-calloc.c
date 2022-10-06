bdd440206366ec68ed4dea6455e390fa7b6e1920
faith-muraya <faith.muraya04@gmail.com>
2022-10-06 04:27:53 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,20 +10,23 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *mem;
-	char *filler;
-	unsigned int index;
+	int i = 0, l = 0;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	mem = malloc(size * nmemb);
+	l = nmemb * size;
+	p = malloc(l);
+
+	if (p == NULL)
 		return (NULL);
 
-	filler = mem;
+	while (i < l)
+	{
+		p[i] =0;
+		i++;
+	}
 
-	for (index = 0; index < (size * nmemb); index++)
-		filler[index] = '\0';
-	
-	return (mem);
+	return (p);
 }
