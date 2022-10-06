1a0ef99871ed7dd4b671826bc2346c075db54845
faith-muraya <faith.muraya04@gmail.com>
2022-10-06 04:23:57 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,7 +7,6 @@
  * @size: The byte size of each array element
  *
  * Return: If nmemb = 0, size = 0, or the function fails - NULL
- * 	Otherwise - a pointer to the allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
