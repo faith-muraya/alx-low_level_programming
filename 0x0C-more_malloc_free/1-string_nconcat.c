993ac061ebed814de3ac660880c45e7c8bea2cc6
faith-muraya <faith.muraya04@gmail.com>
2022-10-06 03:16:15 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,13 +9,16 @@
  * @n: The maximum number of bytes of s2 to concatenate to s1
  *
  * Return: If the function fails - NULL
+ * 	Otherwise - a pointer to the concatenated space in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
 	unsigned int len = n, index;
-
 	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
 		s2 = "";
 
 	for (index = 0; s1[index]; index++)
