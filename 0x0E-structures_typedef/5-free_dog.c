4bdcf98150bb066ce0d753ddb171e3469820f14b
faith-muraya <faith.muraya04@gmail.com>
2022-10-11 05:24:08 -0700

    Task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,13 +4,15 @@
 /**
  * free_dog - Frees dog.
  * @d: The dog to be freed.
+ * Return: nothing
  */
+void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-
-	free(d->owner);
-	free(d->name);
-	free(d);
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
 }
 
