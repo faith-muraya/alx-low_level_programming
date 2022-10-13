76b8503bb42ad1d6c461063d0593fd31247be9b6
faith-muraya <faith.muraya04@gmail.com>
2022-10-13 09:04:49 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
-#include <stlib.h>
 #include "function_pointers.h"
-
 /**
  * print_name - prints a name
  * @name: The name to prints
  * @f: Pointer to function
+ * Return:nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
