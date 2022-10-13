f13faac680effe534baad8b220b82063be25fd46
faith-muraya <faith.muraya04@gmail.com>
2022-10-13 09:10:20 -0700

    Task 1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include <stddio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function given as a parameter
+ * @array: array
+ * @sixe: size
+ * @action:action
+ *
+ **/
+void array_iterator(int *array, size_t size, void (*action)(int)
+{
+	size_t i;
+
+	if ((array == NULL) || (size == 0) || (action == NULL))
+	return:
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
