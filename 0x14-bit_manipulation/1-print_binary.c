492594cf8123f8e4d9540153819771ab33d257e5
faith-muraya <faith.muraya04@gmail.com>
2022-10-31 13:05:34 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation of a number
+ * print_binary - prints the binary representation of a number.
  * @n: unsigned long int.
+ *
  * Return: no return.
  /
 void print_binary(unsigned long int n)
