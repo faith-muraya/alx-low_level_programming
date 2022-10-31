a4c1217333d104fcb0349b07825049a7a86279e7
faith-muraya <faith.muraya04@gmail.com>
2022-10-31 03:34:36 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- * binary_to_uint - converts a binary number to an unsigned int.
- * @b: binary.
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: binary
  *
- * Return: unsigned int.
+ * Return: unsigned int
  */
 unsigned int binary_to_uint(const char *b)
 {
