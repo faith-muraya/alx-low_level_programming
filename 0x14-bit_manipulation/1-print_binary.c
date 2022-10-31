f0ae5ce6739420ede57ce6383141b716498d9494
faith-muraya <faith.muraya04@gmail.com>
2022-10-31 12:42:00 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,19 +3,33 @@
 /**
  * print_binary - prints the binary representation of a number
  * @n: unsigned long int.
- *
  * Return: no return.
  /
 void print_binary(unsigned long int n)
 {
-	if (n >> 0)
-	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
-	}
-	else
+	unsigned long int copy, i, k;
+
+	copy = n;
+	i = 0;
+	if (n == 0)
 	{
 		_putchar('0');
+		return;
+	}
+	while (copy != 0)
+	{
+		i++;
+		copy = copy >> 1;
+	}
+	while (i)
+	{
+	copy = n;
+	for (k = 1; k < i; k++)
+	copy = copy >> 1;
+		if (copy == (copy | 1))
+		_putchar('1');
+		else
+	_putchar ('0');
+		i--;
 	}
 }
