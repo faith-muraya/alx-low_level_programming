153ed1353c7b7be092f1caa8103be6b9dfaabe14
faith-muraya <faith.muraya04@gmail.com>
2022-11-02 13:23:24 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,37 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 /**
  * read_textfile - reads a text file and prints it to standard out
  * @filename: name of file
  * @letters: how many letters to read and print
+ *
  * Return: 0 if error, otherwise number letters printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fi, re, wr;
-	char *buf;
+	ssize_t o, r, w;
+	char *buffer;	
 
-	if (!filename || letters == 0)
+	if (filename == NULL)
 		return (0);
-	fi = open(filename, O_RDONLY);
 
-	if (fi < 0)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		close(fi);
+		free(buffer);
 		return (0);
 	}
-	re = read(fi, buf, letters);
-	close(fi);
-	if (re < 0)
-		{	
-			free(buf);
-			return (0);
-		}
-	wr = write(STDOUT_FILENO, buf, re);
-	if (wr <= 0)
-	{
-		free(buf);
-		return (0);
-	}
-		free(buf);
-		return (wr);
+
+	free(buffer);
+	close(o);
+
+	return (w);
 }
